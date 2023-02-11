7f5aebcb284f38c733bbd0e4cee9fcb3d0508ad4
Akshit Singh Chauhan <84867574+Nzarite@users.noreply.github.com>
2023-02-11 13:37:39 +0530

    Assignment 1 added

--- Assignment 1/main.cpp
+++ Assignment 1/main.cpp
@@ -1,17 +1,3 @@
-// -------------------------------------------------------------- -*- C++ -*-
-// File: cutstock.cpp
-// Version 22.1.0
-// --------------------------------------------------------------------------
-// Licensed Materials - Property of IBM
-// 5725-A06 5725-A29 5724-Y48 5724-Y49 5724-Y54 5724-Y55 5655-Y21
-// Copyright IBM Corporation 2000, 2022. All Rights Reserved.
-//
-// US Government Users Restricted Rights - Use, duplication or
-// disclosure restricted by GSA ADP Schedule Contract with
-// IBM Corp.
-// --------------------------------------------------------------------------
-//
-
 #include <ilcplex/ilocplex.h>
 #include <vector>
 #include <fstream>
